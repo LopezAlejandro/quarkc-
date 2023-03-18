e0fa7a10de4a56dd3347db57a2ecec240e0e13be
Alejandro <lopalejandro@gmail.com>
2023-03-17 23:49:44 -0300

    estructura del mayor menor

---
+++ POO/MayorMenor/Problema1/src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class mayorMenor
+{
+private:
+    /* data */
+public:
+    void carga();
+    void imprime();
+};
+
+void mayorMenor::carga()
+{
+}
+
+void mayorMenor::imprime()
+{
+}
+
+int main()
+{
+    mayorMenor bol1;
+    bol1.carga();
+    bol1.imprime();
+
+    return 0;
+}
