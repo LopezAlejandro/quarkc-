56594e227027e10d02c7c3fd32846de9b4def53d
Alejandro <lopalejandro@gmail.com>
2023-03-18 00:14:56 -0300

    Ejercicio mayot menor completo

--- POO/MayorMenor/Problema1/src/main.cpp
+++ POO/MayorMenor/Problema1/src/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class mayorMenor
 {
 private:
-    /* data */
+    int elementos[4];
 public:
     void carga();
     void imprime();
@@ -12,10 +12,37 @@ public:
 
 void mayorMenor::carga()
 {
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "\nIngrese el valor "<<i<<" : ";
+        cin >> elementos[i];
+    }
+    
 }
 
 void mayorMenor::imprime()
 {
+    int menor;
+    int repe = 0;
+    menor = elementos[0];
+    for (int i = 1; i < 5; i++)
+    {
+        if (elementos[i]< menor)
+        {
+            menor = elementos[i];
+        }
+        else if (elementos[i] == menor)
+        {
+            repe=1;
+        }
+    }
+    cout<< "El menor valor es "<<menor<<"\n";
+
+    if (repe != 0)
+    {
+        cout << "El valor esta repetido";
+    }
+    
 }
 
 int main()
